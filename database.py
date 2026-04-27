c638473c1626ae34cc81f8c8d325b9beeb85a1ad
unknown <Tejas Pawar>
2026-04-27 10:53:31 +0530

    Removed Hardcoded password, using env Variable

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:Tejas%40123@localhost/ads_tracker"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
